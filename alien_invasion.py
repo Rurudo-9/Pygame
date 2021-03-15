b0a3e8deb58a0f60d3a6bbe7e16b233e0252fd29
Jingheng Li <Jinghen_Li@baylor.edu>
2021-03-15 05:42:08 -0500

    limited teh bullets #

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,7 @@ import sys
 import pygame   
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 class AlienInvasion:
     # Overall class to manage game assets and behavior.
@@ -12,9 +13,12 @@ class AlienInvasion:
         self.settings = Settings()
 
         # create a display window. 
-        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
+        self.screen = pygame.display.set_mode((0,0),pygame.FULLSCREEN)
+        self.settings.screen_width = self.screen.get_rect().width
+        self.settings.screen_height = self.screen.get_rect().height
         pygame.display.set_caption("Alien Invasion") 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
         # set the background color.
         self.bg_color = (230,230,230)
@@ -24,8 +28,16 @@ class AlienInvasion:
         while True:
             self._check_events()
             self.ship.update()
-            self._update_screen()
+            self.bullets.update()
             
+            # get rid of bullets that have disappeared
+            for bullet in self.bullets.copy():
+                if bullet.rect.bottom <=0:
+                    self.bullets.remove(bullet)
+            print(len(self.bullets))
+
+            self._update_screen()
+
     
     def _check_events(self):
         #respond to keypresses and mouse events
@@ -47,6 +59,8 @@ class AlienInvasion:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
     
     def _check_keyup_events(self,event):
         #respond to key releases
@@ -55,11 +69,20 @@ class AlienInvasion:
         elif event.key == pygame.K_LEFT:
             Self.ship.moving_left = False
     
+    def _fire_bullet(self):
+        #create a new bullet and add it to the bullets group
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+
+
     def _update_screen(self):
         #update images on the screen, and flip to the new screen
         # redraw the screen during each pass through the loop.
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()  
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
 
 
         # make the most recently drawn screen visible.
