67f852a324e3115911f4008a4e4bcb18fbb86bdb
Jingheng Li <Jinghen_Li@baylor.edu>
2021-03-15 05:22:02 -0500

    _check_events

--- alien_invasion.py
+++ alien_invasion.py
@@ -35,8 +35,8 @@ class AlienInvasion:
                 sys.exit()
             elif event.type == pygame.KEYDOWN:
                 self._check_keydown_events(event)
-                elif event.type == pygame.KEYUP:
-                    self._check_keyup_events(event)
+            elif event.type == pygame.KEYUP:
+                self._check_keyup_events(event)
                     
     
     def _check_keydown_events(self,event):
