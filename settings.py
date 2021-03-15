6258baa48f9bfea361d4dc9197663ad32d121b19
Jingheng Li <Jinghen_Li@baylor.edu>
2021-03-15 04:48:11 -0500

    draw the ship to the screen

--- settings.py
+++ settings.py
@@ -1,10 +1,9 @@
 class Settings:
     # a class to store all settings for Alien Invasion
-
     def __init__(self):
         # initialize the game's settings
         #Screen settings
 
-    self.screen_width = 1200
-    self.screen_height = 800
-    self.screen_bg_color = (230,230,230)
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230,230,230)
